2a0e6dc3344c435c4f5b505775469fbf7ca2dcc6
AsaGrote <agrote@calpoly.edu>
2024-05-06 23:57:55 -0700

    Added user.py

---
+++ src/api/user.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends, Request
+from pydantic import BaseModel
+from src.api import auth
+
+import sqlalchemy
+from src import database as db
+
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"],
+    dependencies=[Depends(auth.get_api_key)],
+)
+
+class User(BaseModel):
+    username: str
+
+@router.post("/")
+def create_user(new_user: User):
+    with db.engine.begin() as connection:
+        
+        result = connection.execute(sqlalchemy.text(
+            f"""
+            SELECT COUNT(*) 
+            FROM users 
+            WHERE username = '{new_user.username}'
+            """)
+        ).scalar_one()
+        
+        if result != 0:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="Username already exists"
+            ) 
+        
+        result = connection.execute(sqlalchemy.text(
+            f"""
+            INSERT INTO users (username)
+            VALUES ('{new_user.username}')""")
+        )
+        
+    return "OK"
+    
