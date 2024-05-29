becd95052289e3db863d91b6db1be8454124b746
Joshua Cheruvelil <cheruvel@calpoly.edu>
2024-05-29 11:50:10 -0700

    remove duplicate rec func

--- src/api/recs.py
+++ src/api/recs.py
@@ -17,7 +17,7 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
-@router.get("/")
+@router.get("/{track_id}")
 def get_rec(user_id: int, track_id: str):
     input_stmt = """
                 SELECT features_vector
@@ -139,61 +139,6 @@ def get_rec_from_ratings(user_id: int):
             "recommendations": recs
         }
     
-
-@router.get("/")
-def get_rec(user_id: int, track_id: str):
-    input_stmt = """
-                SELECT features_vector
-                FROM tracks
-                WHERE track_id = :track_id
-                """
-
-    recs_stmt = """
-        WITH input_vector AS (
-            SELECT features_vector
-            FROM tracks
-            WHERE track_id = :track_id
-        )
-        SELECT
-            t.track_id,
-            t.track_name,
-            t.album_name,
-            t.artists,
-            1 - (t.features_vector <=> input_vector.features_vector) AS similarity
-        FROM tracks t, input_vector
-        WHERE t.track_id != :track_id
-        ORDER BY similarity DESC
-        LIMIT 10;
-    """
-    
-    with db.engine.begin() as connection:
-        # Get the input vector
-        input_vector = connection.execute(sqlalchemy.text(input_stmt), {"track_id": track_id}).fetchone()
-
-        # If input_vector is None, it means the track_id is not found
-        if input_vector is None:
-            return []
-        
-
-        recs = []
-        result = connection.execute(sqlalchemy.text(recs_stmt), [{"track_id": track_id}]).fetchall()
-
-        connection.execute(sqlalchemy.text("""
-                                           INSERT INTO recommendation_history (user_id, query)
-                                           VALUES (:user_id, :input_track)"""),
-                                           {"user_id": user_id, "input_track": track_id})
-
-        for row in result:
-            recs.append({
-                    "id": row.track_id,
-                    "track": row.track_name,
-                    "album": row.album_name,
-                    "artist": row.artists,
-            })
-
-        return{
-            "recommendations": recs
-        }
         
 @router.get("/{playlist_id}")
 def get_rec_from_playlist(playlist_id: int):
